5c54145f73b792407e8379c803c5c5ea77325fc7
Mukul Saini <saini31mukul@gmail.com>
2026-05-14 19:57:20 +0530

    feat(api): wire DI, Swagger, HTTP logging, CORS, exception middleware

--- src/EventManagement.Api/Program.cs
+++ src/EventManagement.Api/Program.cs
@@ -1,17 +1,45 @@
+using EventManagement.Api.Middleware;
+using EventManagement.Infrastructure;
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Event Management API", Version = "v1" });
+});
+builder.Services.AddEventManagement();
+
+builder.Services.AddHttpLogging(o =>
+{
+    o.LoggingFields = HttpLoggingFields.RequestMethod
+                    | HttpLoggingFields.RequestPath
+                    | HttpLoggingFields.ResponseStatusCode
+                    | HttpLoggingFields.Duration;
+});
+
+const string CorsPolicy = "AllowLocalDev";
+builder.Services.AddCors(o => o.AddPolicy(CorsPolicy, p =>
+    p.WithOrigins("http://localhost:5173")
+     .AllowAnyHeader()
+     .AllowAnyMethod()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseHttpLogging();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(CorsPolicy);
+}
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
